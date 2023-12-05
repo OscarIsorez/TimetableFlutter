39b3ef64c30aeee053124192e1f04dbd7adc311c
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2023-12-05 20:56:25 +0100

    ajustements

--- timetableapp/lib/components/Timetable.dart
+++ timetableapp/lib/components/Timetable.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:timetableapp/components/Event.dart';
 import 'package:timetableapp/components/WeeklySchedule.dart';
 import 'package:http/http.dart' as http;
-import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'package:icalendar_parser/icalendar_parser.dart';
-import 'package:icalendar/icalendar.dart' as ical;
 
 class Timetable {
   final String url;
   String ics_String = "";
   Map<int, WeeklySchedule> weeklySchedules = {};
+
 
   List<Event> events = [];
 
@@ -59,12 +58,12 @@ class Timetable {
     final file = File('${appStorage.path}/data.json');
     final jsonData = jsonDecode(file.readAsStringSync());
     print(jsonData);
-    for (var event in jsonData['events']) {
+    for (var event in jsonData['data']) {
       events.add(Event(
           summary: event['summary'],
           description: event['description'],
-          start: DateTime.parse(event['start']),
-          end: DateTime.parse(event['end']),
+          start: DateTime.parse(event['dtstart']['dt']),
+          end: DateTime.parse(event['dtend']['dt']),
           location: event['location']));
     }
   }
@@ -73,17 +72,20 @@ class Timetable {
     /*
       @return the week of the date d (an int between 1 and 52)  
      */
-
-    int week = 1;
+    int weekOfTheDate = 1;
     DateTime date = DateTime(d.year, 1, 1);
-    while (date.weekday != 1) {
-      date = date.add(Duration(days: 1));
-    }
     while (date != d) {
-      date = date.add(Duration(days: 7));
-      week++;
+      date = date.add(const Duration(days: 1));
+      if (date.weekday == 1) {
+        weekOfTheDate++;
+      }
+      if (weekOfTheDate == 53) {
+        weekOfTheDate = 1;
+        break;
+      }
     }
-    return week;
+
+    return weekOfTheDate;
   }
 
   Future<void> fillWeeklySchedules() async {
@@ -102,4 +104,116 @@ class Timetable {
       }
     }
   }
+
+
+  
+  Widget buildScheduleList() {
+
+    List<String> days = ['S1', 'S2', 'S3', 'S4', 'S5'];
+
+    // return 
+    //   ListView.builder(
+    //     scrollDirection: Axis.horizontal,
+    //     itemCount: timetable.weeklySchedules.length,
+    //     itemBuilder: (context, index) {
+    //       return buildWeeklySchedule(timetable.weeklySchedules[index]!);
+    //     });
+
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: days.length,
+      itemBuilder: (context, index) {
+        return Text(days[index]);
+
+      });
+    }
+  
+  
+
+  Widget buildDay(List<Event> day) {
+    /* 
+      will return a column. this column will be composed with grey spaces, and the events
+      we will start the day at 8.0, and finish at 19.0. we will increment by O.25
+      if the event equals the hour, we will add the event and will calculate the height of the event by the difference between the start and the end ( modulo 0.25)
+      if the event is not equal to the hour, we will add a grey space and increment the hour by 0.25
+
+      for intance: day = [coursefrom8h15To9h, coursefrom9h15To10h]
+      we will have a column starting with one grey space, then the first event which will be 3 grey spaces high, then 1 grey space, then the second event which will be 3 grey spaces high, then 36 grey spaces bc we are at 10h and we need to go to 19h
+      antoher instance: day = [coursefrom10to11h30] we will start with 8 grey spaces, then the event which will be 6 grey spaces high, then 30 grey spaces bc we are at 11h30 and we need to go to 19h
+     */
+
+    // We will start at 8.0 and finish at 19.0
+    double hour = 8.0;
+
+    // We will increment by 0.25
+    double increment = 0.25;
+
+    
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+        ),
+        padding: const EdgeInsets.all(1),
+        child: Column(
+          children: [
+            for (var event in day)
+              Column(
+                children: [
+                  // Grey spaces
+                  Container(
+                    height: (event.start.hour - hour) * 4 * 7,
+                    width: 100,
+                    color: Colors.grey[300],
+                  ),
+
+                  // Event
+                  Container(
+                    height: (event.end.hour - event.start.hour) * 4 * 7,
+                    width: 100,
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(
+                        event.summary,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Grey spaces
+                  Container(
+                    height: (19.0 - event.end.hour) * 4 * 7,
+                    width: 100,
+                    color: Colors.grey[300],
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+
+
+
+
+
+    
+
+
+
+  }
+
+  Widget buildWeeklySchedule(WeeklySchedule weeklySchedule) {
+    return SizedBox(
+      width: 100,
+      child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return buildDay(weeklySchedule.events[index]);
+          }),
+    );
+  }
+
 }
--- timetableapp/lib/pages/home_page.dart
+++ timetableapp/lib/pages/home_page.dart
@@ -1,16 +1,4 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:icalendar_parser/icalendar_parser.dart';
-import 'package:open_file/open_file.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:timetableapp/components/Event.dart';
-import 'package:timetableapp/components/Timetable.dart';
-import 'package:http/http.dart' as http;
-import 'package:timetableapp/components/WeeklySchedule.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -20,112 +8,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Timetable timetable = Timetable(
-      url:
-          'https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/o35ex53R.shu');
-
-  @override
-  void initState() {
-    super.initState();
-
-    timetable.loadData();
-    timetable.fillWeeklySchedules();
-  }
-
-  Widget buildScheduleList() {
-    return ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: timetable.weeklySchedules.length,
-        itemBuilder: (context, index) {
-          return buildWeeklySchedule(timetable.weeklySchedules[index]!);
-        });
-  }
-
-  Widget buildDay(List<Event> day) {
-    /* 
-      will return a column. this column will be composed with grey spaces, and the events
-      we will start the day at 8.0, and finish at 19.0. we will increment by O.25
-      if the event equals the hour, we will add the event and will calculate the height of the event by the difference between the start and the end ( modulo 0.25)
-      if the event is not equal to the hour, we will add a grey space and increment the hour by 0.25
-
-      for intance: day = [coursefrom8h15To9h, coursefrom9h15To10h]
-      we will have a column starting with one grey space, then the first event which will be 3 grey spaces high, then 1 grey space, then the second event which will be 3 grey spaces high, then 36 grey spaces bc we are at 10h and we need to go to 19h
-      antoher instance: day = [coursefrom10to11h30] we will start with 8 grey spaces, then the event which will be 6 grey spaces high, then 30 grey spaces bc we are at 11h30 and we need to go to 19h
-     */
-
-    // We will start at 8.0 and finish at 19.0
-    double hour = 8.0;
-
-    // We will increment by 0.25
-    double increment = 0.25;
-
-    
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[300],
-        ),
-        padding: const EdgeInsets.all(1),
-        child: Column(
-          children: [
-            for (var event in day)
-              Column(
-                children: [
-                  // Grey spaces
-                  Container(
-                    height: (event.start.hour - hour) * 4 * 7,
-                    width: 100,
-                    color: Colors.grey[300],
-                  ),
-
-                  // Event
-                  Container(
-                    height: (event.end.hour - event.start.hour) * 4 * 7,
-                    width: 100,
-                    color: Colors.blue,
-                    child: Center(
-                      child: Text(
-                        event.summary,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  // Grey spaces
-                  Container(
-                    height: (19.0 - event.end.hour) * 4 * 7,
-                    width: 100,
-                    color: Colors.grey[300],
-                  ),
-                ],
-              ),
-          ],
-        ),
-      ),
-    );
-
-
-
-
-
-    
-
-
-
-  }
-
-  Widget buildWeeklySchedule(WeeklySchedule weeklySchedule) {
-    return Container(
-      width: 100,
-      child: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return buildDay(weeklySchedule.events[index]);
-          }),
-    );
-  }
+  List<String> lst = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+  PageController _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
@@ -136,27 +20,18 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(
             icon: const Icon(Icons.launch),
             onPressed: () => {
-              timetable.loadData(),
-              timetable.fillWeeklySchedules(),
+              setState(() {}),
             },
           ),
         ],
       ),
-      body:
-          // padding: const EdgeInsets.all(16.0),
-          Column(
+      body: Column(
         children: [
           // Days
           Row(
             children: [
               const SizedBox(width: 65), // Case vide avant le lundi
-              for (var day in [
-                'Lundi',
-                'Mardi',
-                'Mercredi',
-                'Jeudi',
-                'Vendredi'
-              ])
+              for (var day in ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'])
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
@@ -164,12 +39,13 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     padding: const EdgeInsets.all(1),
                     child: Center(
-                        child: Text(
-                      day,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
+                      child: Text(
+                        day,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    )),
+                    ),
                   ),
                 ),
             ],
@@ -179,62 +55,69 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    // height: HEIGHT,
-                    color: Colors.grey[300],
-                    child: Column(
-                      children: [
-                        for (var hour in [
-                          '8:00',
-                          '9:00',
-                          '10:00',
-                          '11:00',
-                          '12:00',
-                          '13:00',
-                          '14:00',
-                          '15:00',
-                          '16:00',
-                          '17:00',
-                          '18:00',
-                          '19:00',
-                        ])
-                          Column(
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(2),
-                                margin:
-                                    const EdgeInsets.only(bottom: 2, left: 5),
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Colors.grey[300],
-                                ),
-                                child: Text(
-                                  hour,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                Container(
+                  width: 65,
+                  color: Colors.grey[300],
+                  child: Column(
+                    children: [
+                      for (var hour in [
+                        '8:00',
+                        '9:00',
+                        '10:00',
+                        '11:00',
+                        '12:00',
+                        '13:00',
+                        '14:00',
+                        '15:00',
+                        '16:00',
+                        '17:00',
+                        '18:00',
+                        '19:00',
+                      ])
+                
+                        // Hours
+                        Column(
+                          children: [
+                            Container(
+                              color: Colors.grey[100],
+                              padding: const EdgeInsets.all(2),
+                              margin: const EdgeInsets.only(bottom: 2, left: 5),
+                              alignment: Alignment.center,
+                              child: Text(
+                                hour,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Container(
-                                height: 28,
-                                width: 5,
-                                color: Colors.black,
-                              ),
-                            ],
-                          ),
-                      ],
-                    ),
+                            ),
+                            Container(
+                              height: 28,
+                              width: 5,
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                    ],
                   ),
                 ),
+
+                // TimeTable
                 Expanded(
-                  flex: 5, // Cette colonne prend plus de place
-                  child: Container(
-                    // height: HEIGHT,
-                    color: Colors.grey,
-                    child: buildScheduleList(),
+                  flex: 5,
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: lst.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        child: Center(child: Text(lst[index])),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
               ],
