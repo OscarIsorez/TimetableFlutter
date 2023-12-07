4e8e8b7257d3e706de6c607398157775d7095572
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2023-12-07 11:13:00 +0100

    add more stuff

--- timetableapp/lib/components/Timetable.dart
+++ timetableapp/lib/components/Timetable.dart
@@ -1,3 +1,4 @@
+import 'package:timetableapp/components/WeeklySchedule.dart';
 import 'package:flutter/material.dart';
 import 'package:timetableapp/components/Event.dart';
 import 'package:timetableapp/components/WeeklySchedule.dart';
@@ -8,29 +9,71 @@ import 'dart:io';
 import 'package:icalendar_parser/icalendar_parser.dart';
 
 class Timetable {
-  final String url;
-  String ics_String = "";
-  Map<int, WeeklySchedule> weeklySchedules = {};
+  String url = "";
+  List<WeeklySchedule> schedules = [];
 
-
-  List<Event> events = [];
+  // ------------------ CONSTRUCTOR ------------------ //
 
   Timetable({required this.url});
 
-  Future<void> loadData() async {
+  // ------------------ METHODS ------------------ //
+
+  generateInitSchedles() {
+
+    List<Event> events = [
+      Event(
+          summary: "the summary",
+          description: " the description",
+          location: "the location",
+          start: DateTime.now(),
+          end: DateTime.now()),
+      Event(
+          summary: "the summary",
+          description: " the description",
+          location: "the location",
+          start: DateTime.now(),
+          end: DateTime.now()),
+    ];
+    WeeklySchedule weekySh = WeeklySchedule(
+      monday: events,
+      tuesday: events,
+      wednesday: events,
+      thursday: events,
+      friday: events,
+    );
+    for (var i = 0; i < 10; i++) {
+      schedules.add(weekySh);
+    }
+   
+  }
+
+  Future<List<WeeklySchedule>> generateTimetable() async {
+    //  extracting data from ics file
+    //  and converting it to a list of events
+    //  and then adding it to the schedule
     final response = await http.get(Uri.parse(url));
 
     final body = response.body;
 
     final file = await writeICSData(body);
 
-    // final openFile = await OpenFile.open(file.path);
-
+    // we fill  the list of events from file
     final icsObj = ICalendar.fromLines(File(file.path).readAsLinesSync());
 
-    final jsonfile = await writeJSONData(jsonEncode(icsObj.toJson()));
+    // for (var i = 0; i < icsObj.events.length; i++) {
+    //   events.add(Event(
+    //       start: icsObj.events[i].dtstart,
+    //       end: icsObj.events[i].dtend,
+    //       location: icsObj.events[i].location,
+    //       description: icsObj.events[i].description,
+    //       summary: icsObj.events[i].summary));
+    // }
 
-    // final openFile = await OpenFile.open(jsonfile.path);
+    
+    // we fill the schedule with the events
+    generateInitSchedles();
+    
+    return schedules;
   }
 
   Future<File> writeICSData(String data) async {
@@ -39,181 +82,4 @@ class Timetable {
     await file.writeAsString(data);
     return file;
   }
-
-  Future<File> writeJSONData(String data) async {
-    final appStorage = await getApplicationDocumentsDirectory();
-    final file = File('${appStorage.path}/data.json');
-    await file.writeAsString(data);
-    return file;
-  }
-
-  Future<void> convertICStoString() async {
-    final appStorage = await getApplicationDocumentsDirectory();
-    final file = File('${appStorage.path}/data.ics');
-    ics_String = file.readAsStringSync();
-  }
-
-  Future<void> convertToListOfEvents() async {
-    final appStorage = await getApplicationDocumentsDirectory();
-    final file = File('${appStorage.path}/data.json');
-    final jsonData = jsonDecode(file.readAsStringSync());
-    print(jsonData);
-    for (var event in jsonData['data']) {
-      events.add(Event(
-          summary: event['summary'],
-          description: event['description'],
-          start: DateTime.parse(event['dtstart']['dt']),
-          end: DateTime.parse(event['dtend']['dt']),
-          location: event['location']));
-    }
-  }
-
-  int getWeekOfTheDate(DateTime d) {
-    /*
-      @return the week of the date d (an int between 1 and 52)  
-     */
-    int weekOfTheDate = 1;
-    DateTime date = DateTime(d.year, 1, 1);
-    while (date != d) {
-      date = date.add(const Duration(days: 1));
-      if (date.weekday == 1) {
-        weekOfTheDate++;
-      }
-      if (weekOfTheDate == 53) {
-        weekOfTheDate = 1;
-        break;
-      }
-    }
-
-    return weekOfTheDate;
-  }
-
-  Future<void> fillWeeklySchedules() async {
-    await convertToListOfEvents();
-
-    final DateTime date = DateTime.now();
-
-    for (var event in events) {
-      int weekOfthedate = getWeekOfTheDate(date);
-      if (weeklySchedules.containsKey(weekOfthedate)) {
-        weeklySchedules[weekOfthedate]!.addEvent(event);
-      } else {
-        weeklySchedules[weekOfthedate] = WeeklySchedule(
-            monday: [], tuesday: [], wednesday: [], thursday: [], friday: []);
-        weeklySchedules[weekOfthedate]!.addEvent(event);
-      }
-    }
-  }
-
-
-  
-  Widget buildScheduleList() {
-
-    List<String> days = ['S1', 'S2', 'S3', 'S4', 'S5'];
-
-    // return 
-    //   ListView.builder(
-    //     scrollDirection: Axis.horizontal,
-    //     itemCount: timetable.weeklySchedules.length,
-    //     itemBuilder: (context, index) {
-    //       return buildWeeklySchedule(timetable.weeklySchedules[index]!);
-    //     });
-
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemCount: days.length,
-      itemBuilder: (context, index) {
-        return Text(days[index]);
-
-      });
-    }
-  
-  
-
-  Widget buildDay(List<Event> day) {
-    /* 
-      will return a column. this column will be composed with grey spaces, and the events
-      we will start the day at 8.0, and finish at 19.0. we will increment by O.25
-      if the event equals the hour, we will add the event and will calculate the height of the event by the difference between the start and the end ( modulo 0.25)
-      if the event is not equal to the hour, we will add a grey space and increment the hour by 0.25
-
-      for intance: day = [coursefrom8h15To9h, coursefrom9h15To10h]
-      we will have a column starting with one grey space, then the first event which will be 3 grey spaces high, then 1 grey space, then the second event which will be 3 grey spaces high, then 36 grey spaces bc we are at 10h and we need to go to 19h
-      antoher instance: day = [coursefrom10to11h30] we will start with 8 grey spaces, then the event which will be 6 grey spaces high, then 30 grey spaces bc we are at 11h30 and we need to go to 19h
-     */
-
-    // We will start at 8.0 and finish at 19.0
-    double hour = 8.0;
-
-    // We will increment by 0.25
-    double increment = 0.25;
-
-    
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[300],
-        ),
-        padding: const EdgeInsets.all(1),
-        child: Column(
-          children: [
-            for (var event in day)
-              Column(
-                children: [
-                  // Grey spaces
-                  Container(
-                    height: (event.start.hour - hour) * 4 * 7,
-                    width: 100,
-                    color: Colors.grey[300],
-                  ),
-
-                  // Event
-                  Container(
-                    height: (event.end.hour - event.start.hour) * 4 * 7,
-                    width: 100,
-                    color: Colors.blue,
-                    child: Center(
-                      child: Text(
-                        event.summary,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  // Grey spaces
-                  Container(
-                    height: (19.0 - event.end.hour) * 4 * 7,
-                    width: 100,
-                    color: Colors.grey[300],
-                  ),
-                ],
-              ),
-          ],
-        ),
-      ),
-    );
-
-
-
-
-
-    
-
-
-
-  }
-
-  Widget buildWeeklySchedule(WeeklySchedule weeklySchedule) {
-    return SizedBox(
-      width: 100,
-      child: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return buildDay(weeklySchedule.events[index]);
-          }),
-    );
-  }
-
 }
--- timetableapp/lib/pages/home_page.dart
+++ timetableapp/lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:timetableapp/components/Event.dart';
+import 'package:timetableapp/components/Timetable.dart';
+import 'package:timetableapp/components/WeeklySchedule.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,18 +12,84 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<String> lst = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-  PageController _pageController = PageController(initialPage: 0);
+  final PageController _pageController = PageController(initialPage: 0);
+  String appBarTitle = 'Schedule';
+  List<WeeklySchedule> schedules = [];
+  Timetable timetable = Timetable(url: "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/o35ex53R.shu");
+
+  Event ev1 = Event(
+      start: DateTime(2021, 10, 11, 8, 0),
+      end: DateTime(2021, 10, 11, 9, 0),
+      location: 'Room 1',
+      description: 'Maths class',
+      summary: "test111");
+
+  Event ev2 = Event(
+      start: DateTime(2021, 10, 11, 9, 0),
+      end: DateTime(2021, 10, 11, 10, 0),
+      location: 'Room 2',
+      description: 'English class',
+      summary: "test2");
+
+  Event ev3 = Event(
+      start: DateTime(2021, 10, 11, 10, 0),
+      end: DateTime(2021, 10, 11, 11, 0),
+      location: 'Room 3',
+      description: 'French class',
+      summary: "test3");
+
+  Event ev4 = Event(
+      start: DateTime(2021, 10, 11, 11, 0),
+      end: DateTime(2021, 10, 11, 12, 0),
+      location: 'Room 4',
+      description: 'History class',
+      summary: "test4");
+
+
+  WeeklySchedule generateSchedle() {
+    WeeklySchedule weekySh = WeeklySchedule(
+      monday: [ev1, ev2, ev3, ev4],
+      tuesday: [ev1],
+      wednesday: [ev1, ev4],
+      thursday: [ev1, ev2, ev3, ev4],
+      friday: [ev1, ev3, ev4],
+    );
+    return weekySh;
+  }
+
+  Future<void> updateMultipleSchedules() async {
+    // setState(() {
+    //   schedules = [];
+    //   for (var i = 0; i < 10; i++) {
+
+    //     WeeklySchedule wkShuflled  = generateSchedle();
+    //     wkShuflled.monday.shuffle();
+    //     schedules.add( wkShuflled);
+    // }
+    // });
+    var tpSchudles = await timetable.generateTimetable();
+    
+    setState(() {
+      schedules = tpSchudles;  
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    updateMultipleSchedules();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Weekly Schedule'),
+        title: Text(appBarTitle),
         actions: [
           IconButton(
             icon: const Icon(Icons.launch),
             onPressed: () => {
-              setState(() {}),
+              updateMultipleSchedules(),
             },
           ),
         ],
@@ -31,7 +100,13 @@ class _MyHomePageState extends State<MyHomePage> {
           Row(
             children: [
               const SizedBox(width: 65), // Case vide avant le lundi
-              for (var day in ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'])
+              for (var day in [
+                'Lundi',
+                'Mardi',
+                'Mercredi',
+                'Jeudi',
+                'Vendredi'
+              ])
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
@@ -74,12 +149,12 @@ class _MyHomePageState extends State<MyHomePage> {
                         '18:00',
                         '19:00',
                       ])
-                
+
                         // Hours
                         Column(
                           children: [
                             Container(
-                              color: Colors.grey[100],
+                              color: Colors.grey[300],
                               padding: const EdgeInsets.all(2),
                               margin: const EdgeInsets.only(bottom: 2, left: 5),
                               alignment: Alignment.center,
@@ -106,17 +181,38 @@ class _MyHomePageState extends State<MyHomePage> {
                   flex: 5,
                   child: PageView.builder(
                     controller: _pageController,
-                    itemCount: lst.length,
+                    itemCount: schedules.length,
                     itemBuilder: (context, index) {
-                      return Container(
-                        child: Center(child: Text(lst[index])),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 1,
-                          ),
-                        ),
-                      );
+                      return Column(
+                        children: [
+                          for (var day in schedules[index].events)
+                            Container(
+                              child: Text
+                              (day[0].summary),
+
+                            )
+                          // Container(
+                          //   height: 100,
+                          //   width: 100,
+                          //   color: Colors.blue[300],
+                          //   child: Center(child: 
+                          //   Text(schedules[index].monday[1].summary)
+                          //   ) 
+                          // ),
+                          // Container(
+                          //   color: Colors.red[300],
+                          //    height: 100,
+                          //   width: 100,
+                          //   child: Center(child: 
+                          //   Text(schedules[index].monday[2].summary,)
+                          //   ) 
+                          // ),
+                          
+                        ],
+                          
+
+                          );
+                          
                     },
                   ),
                 ),
