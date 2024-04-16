0c224362ac4a27fabfde9618cb319222bde1659d
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2024-04-16 12:18:53 +0200

    Fix time calculation and initialize time zones in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:timetableapp/components/App_Theme.dart';
 import 'package:timetableapp/pages/home_page.dart';
-import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
 
 void main() {
-  tz.initializeTimeZones();
-
   runApp(const MyApp());
   // SystemChrome.setPreferredOrientations([
   //   DeviceOrientation.portraitUp,
