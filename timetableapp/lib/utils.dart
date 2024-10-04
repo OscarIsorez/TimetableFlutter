b534963b14e85aee61baecb45ab8ad1707c740fe
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2024-10-04 12:28:00 +0100

    Refactor formatTime function to improve time formatting

--- timetableapp/lib/utils.dart
+++ timetableapp/lib/utils.dart
@@ -11,3 +11,7 @@ DateTime getStartOfWeek(DateTime date) {
 
 DateTime get startOfThisWeek => getMonday(DateTime.now());
 DateTime get endOfWeek => startOfThisWeek.add(const Duration(days: 4));
+
+String formatTime(DateTime time) {
+  return '${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}';
+}
