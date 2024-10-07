0ee68fc9bf98044fa58e54d046faaa6f8e720b0a
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2024-10-07 13:01:42 +0100

    Refactor constant.dart to rename Constant class to Constants and add hourHeight constant

--- timetableapp/lib/constants.dart
+++ timetableapp/lib/constants.dart
@@ -1,5 +1,6 @@
-class Constant {
+class Constants {
   static const weekDays = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'];
   static const weekDaysShort = ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven'];
   static const columnWidth = 50.0;
+  static const double hourHeight = 60.0;
 }
